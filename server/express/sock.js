64fb602a954a5caf3c5543a673d4ac2fbeb58bff
Ralph Donato <ralph.donato@nyserda.ny.gov>
2019-10-25 09:38:25 -0400

    added features to sock.js

--- server/express/sock.js
+++ server/express/sock.js
@@ -1,14 +1,22 @@
 var net = require('net');
 var cntr =0;
-var server = net.createServer(function(socket) {
+var server = net.createServer(function(socket){
 	socket.write('Echo server');
+	console.log( socket.remoteAddress);
 	socket.on('data', function(data){
 		console.log("DT-" +data);
 		textChunk = data.toString('utf8');
 		console.log(textChunk);
 		socket.write(textChunk);
-    });
-    socket.on('error', () => console.log('errored'));
+
+	});
+	socket.on('close',(c)=>{
+		console.log('CLosed' );
+		console.log(c );
+	});
+    socket.on('error',(err)=>{
+		console.log('errored') ;
+		console.log(err);
+	});
 });
 server.listen(1337, '127.0.0.1');
-
