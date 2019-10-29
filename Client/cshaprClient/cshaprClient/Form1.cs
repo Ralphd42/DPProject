4b09fdf0405ab5c18a2b74cd062e38c51d53b241
Ralph Donato <ralph.donato@nyserda.ny.gov>
2019-10-29 13:16:08 -0400

    lastFrom Work

--- Client/cshaprClient/cshaprClient/Form1.cs
+++ Client/cshaprClient/cshaprClient/Form1.cs
@@ -12,19 +12,26 @@ namespace cshaprClient
 {
     public partial class Form1 : Form
     {
+        SocketManager sm = new SocketManager();
         public Form1()
         {
             InitializeComponent();
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.textBox1.Text =SocketManager.test();
+           // this.textBox1.Text =SocketManager.test();
         }
 
         private void btnClient_Click(object sender, EventArgs e)
         {
-            this.textBox1.Text = SocketManager.GetCLients();
+            //this.textBox1.Text = SocketManager.GetCLients();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sm.ConnectToServer(this.textBox1);
         }
     }
 }
