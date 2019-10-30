4411b0f9e7100640a665ed00ff2b23c65bdfe532
Ralph Donato <ralph.donato@nyserda.ny.gov>
2019-10-30 15:27:58 -0400

    Some MonteCarlo

--- Client/cshaprClient/cshaprClient/Form1.cs
+++ Client/cshaprClient/cshaprClient/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -31,7 +32,42 @@ namespace cshaprClient
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            sm.ConnectToServer(this.textBox1);
+         //   sm.ConnectToServer(this.textBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            MonteCarloPi p = new MonteCarloPi();
+            double dmax = -9999;
+            double dmin = 999999;
+            long mx =   int.MaxValue;
+            mx = mx * 10;
+            for (long i = 0; i < mx; ++i)
+            {
+                double rand = p.GetRandomNumber(0, 1);
+                if (rand > dmax)
+                {
+                    dmax = rand;
+                    if (i > int.MaxValue)
+                    {
+                        this.textBox1.Text = "dmax = " + dmax.ToString() + " dmin = " + dmin.ToString();
+                        Thread.Sleep(1);
+                    }
+                }
+                if (rand < dmin)
+                {
+                    dmin = rand;
+                    if (i > int.MaxValue)
+                    {
+                        this.textBox1.Text = "dmax = " + dmax.ToString() + " dmin = " + dmin.ToString();
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+            this.textBox1.Text = "dmax = " + dmax.ToString() + " dmin = " + dmin.ToString();
+
+            this.Enabled = true;
         }
     }
 }
---
+++ Client/cshaprClient/cshaprClient/MonteCarloPi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshaprClient
+{
+    class MonteCarloPi
+    {
+        public int NumAttemps{get;set;}
+        public int hits      {get;set;}
+        public void runMC()
+        {
+            for( int i=0; i<NumAttemps; ++i)
+            {
+                double x = 0;
+                double y = 0;
+                //x = Math.
+            
+            
+            
+            
+            
+            }
+        }
+        public double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
