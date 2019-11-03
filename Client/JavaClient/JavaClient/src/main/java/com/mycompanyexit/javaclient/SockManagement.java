ccc4d01671a990b9b89509e04ed89d0518e73985
ralphd42 <rdonato@albany.edu>
2019-11-02 20:56:45 -0400

    formatted java

--- Client/JavaClient/JavaClient/src/main/java/com/mycompanyexit/javaclient/SockManagement.java
+++ Client/JavaClient/JavaClient/src/main/java/com/mycompanyexit/javaclient/SockManagement.java
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 package com.mycompanyexit.javaclient;
-import java.net.*; 
-import java.io.*; 
+
+import java.net.*;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,60 +15,43 @@ import java.util.logging.Logger;
  * @author ralph
  */
 public class SockManagement {
-    DataInputStream dis=null;
-    
-    
-    
-public String ProcL()
-{
-    Thread readMessage = new Thread(new Runnable()  
-        { 
+
+    DataInputStream dis = null;
+
+    public String ProcL() {
+        Thread readMessage = new Thread(new Runnable() {
             @Override
-            public void run() { 
-  
-                while (true) { 
-                    try { 
+            public void run() {
+
+                while (true) {
+                    try {
                         // read the message sent to this client 
-                      //  String msg = dis.readUTF(); 
-                      
-                        System.out.println((char)dis.readByte());
-                        //System.out.println(msg); 
-                        
-                    } catch (IOException e) { 
+                        //  String msg = dis.readUTF(); 
+
+                        System.out.println((char) dis.readByte());
+
+                    } catch (IOException e) {
                         System.out.println("Error");
-                    } 
-                } 
-            } 
-        }); 
-    
-    
-    
-    
-    String retval = "";
-    try {
-        
-        Socket client = new Socket("127.0.0.1", 1337);
-         OutputStream outS = client.getOutputStream();
-         DataOutputStream dos = new DataOutputStream(outS);
-         
-         dos.writeChar('L');//   .writeUTF("L");
-        // InputStream inFromServer = client.getInputStream();
-        dis = new DataInputStream(client.getInputStream()); 
-        //BufferedReader rd = new BufferedReader(new InputStreamReader(inFromServer)); 
-         //retval = in.//in.readUTF();
-        //retval =rd.readLine();
-        readMessage.start();
-        
-    } catch (IOException ex) {
-        Logger.getLogger(SockManagement.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+
+        String retval = "";
+        try {
+
+            Socket client = new Socket("127.0.0.1", 1337);
+            OutputStream outS = client.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(outS);
+
+            dos.writeChar('L');
+            dis = new DataInputStream(client.getInputStream());
+            readMessage.start();
+
+        } catch (IOException ex) {
+            Logger.getLogger(SockManagement.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retval;
     }
-    return retval;
-}
-    
-    
-    
-    
-    
-    
-    
+
 }
