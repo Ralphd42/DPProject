d3e3464a3b695548dcc6ca4458956d2feb70b438
Ralphd42 <rdonato@albany.edu>
2019-12-02 17:59:59 -0500

    hist

--- server/express/MergeServer.js
+++ server/express/MergeServer.js
@@ -11,8 +11,14 @@ var CommTerm  = '|';
 var CommSplit = '+';
 
 // input codes  -- codes received from a client
-var inJoin = 'N';
+var inJoin = 'N';  //  
 var Sorted = 'S';  // sorted String
+// output to client 
+var cMergeJob ='M'; // send merge request to client MTasKID+commaseparatedData|
+
+//input codes from management console
+var newSort ='M';  // new array to Merge Sort
+
 // db connection info
 const dbConn =Object.freeze({
     host: 'localhost',
@@ -20,7 +26,7 @@ const dbConn =Object.freeze({
     password: 'abc123',
     database: 'DPProject'});
 
-console.log("1-");
+ 
 var server = net.createServer(function(socket)
 {
     console.log("Server");
@@ -61,7 +67,9 @@ var server = net.createServer(function(socket)
                             console.log('Connected! to DB');
                             console.log(datastring[0]);
                             console.log(datastring[1]);
-                            socket.write("M+12342+123,45,37,2,5,221|");
+                            // add client
+
+                            //socket.write("M+12342+123,45,37,2,5,221|");
                         }
                     });
                 }else if (
@@ -75,15 +83,159 @@ var server = net.createServer(function(socket)
                     console.log( datastring);
                     console.log( ds);
                     console.log("END Rcvd S");
+                    var parsObject ={};
+                    parseInputSort(datastring, parsObject);
+                    /*
+                        parseout.TaskID =ID;
+                        parseout.SortedData =SortedData;
+                    
+                    */
+                    console.log("----Parsed-Sort-------------");
+                    console.log(parsObject.TaskID);
+                    console.log(parsObject.SortedData);
+                    console.log("----DoneParsed-Sort-------------");
                     /*1) parse
                     2)   update database
                     3) merge
                      */
-                    
+                      
+
+                }else if(
+                    datastring[0]== newSort || 
+                    datastring[1]== newSort ||
+                    datastring   == newSort || 
+                    ds           == newSort
+                    )
+                {
+
+                    var ArrayText = datastring.substring(2,datastring.length-2);
+                    // arrayText = the array - all extras
+                    console.log( ArrayText);
+                    //add to database
+                    //split up and send to clients
+                        // build tasks
+                        // loop connections
+                    //?? acknowledge
+                    /* Add Job to Database */
+                    addMergeSortJob(ArrayText, socket );
+
+
+
+
+
 
                 }
             }
         }
     });
 });
+
+function addMergeSortJob(  MSData, CmdConSocket)
+{
+    const connection = mysql.createConnection(dbConn);
+    connection.connect((err) => 
+    {
+        if (err)
+        {
+            console.log("Failed to connect For Job Creation");
+            CmdConSocket.write("E Failed to Add Job|");
+            CmdConSocket.destroy();
+        }else
+        {
+            connection.query('INSERT INTO DPProject.Jobs Set ?',
+                {JobType: 'M' ,MergeData:  MSData } ,
+                (error, results, fields) => 
+            {
+                if(error) 
+                {
+                    console.log("Failed to Insert into Job");
+                    CmdConSocket.write("AE Failed to ADDJOB to DB|");
+                }else
+                {
+                    console.log('Created  Merge job in DB:\n');
+                    var JobID = results.insertId;
+                    var jobDataArray = MSData.split(',').map(Number);
+                    var lendata =jobDataArray.length;
+                    var numclients =clientSockets.length;
+                    var slicelen = lendata/ (numclients);
+                    var extra = lendata%numclients;
+                        // create tasks
+                    for( i=0;i< numclients ;i++)
+                    {
+                        jobdata = jobDataArray.slice(i*slicelen,  (i+1)*slicelen  );
+                        if(     i==(numclients-1))
+                        {
+                            jobdata = jobDataArray.slice(i*slicelen,  (i+1)*slicelen  +extra   );
+                        }
+                        var jobdataString = jobdata.join(",");
+                        connection.query('INSERT INTO DPProject.Tasks Set ?',{
+                            ClientID: 0 ,
+                            JobID:JobID,
+                            TaskData:  jobdataString },
+                            (error, results, fields) => 
+                            {
+                                if(error) {
+                                    console.log("Failed to Insert into Job");
+                                    CmdConSocket.write("E Failed to Task to DB|");
+                                }else
+                                {
+                                    var TaskID = results.insertId;
+                                    var sortmsg =cMergeJob + TaskID.toString();
+                                    sortmsg +=CommSplit;
+                                    sortmsg += jobdataString;
+                                    sortmsg += CommTerm;
+                                    clientSockets[i].write(sortmsg);
+                                }
+                            }
+                        );
+                    }
+                }
+            });
+        }
+    });
+    
+}
+
+function AddNewClient( sNewClientSock)
+{
+    const connection = mysql.createConnection(dbConn);
+    connection.connect(
+    (err) => 
+    {
+        if (err)
+        {
+            console.log("Failed to connect");
+            socket.write("AE Failed to connect|");
+            socket.destroy();
+        }else
+        {
+            connection.query('INSERT INTO DPProject.Clients Set ?',
+            {IPAddress: sNewClientSock.remoteAddress  } ,
+            (error, results, fields) => 
+            {
+                if(error) 
+                {
+                    console.log("Failed to Insert");
+                    socket.write("AE Failed to ADDCLIENT|");
+                    socket.destroy();
+                }
+                sNewClientSock.write("A"+ results.insertId.toString()+"|");
+                sNewClientSock.ClientID =results.insertId;
+                clientSockets.push(sNewClientSock);
+            });
+        }
+    });
+}
+
+
+function parseInputSort( inputData, parseout)
+{
+    var startTID = inputData.indexOf(Sorted   );
+    var endID   =  inputData.indexOf(CommSplit);
+    var ID = inputData.substring (startTID+1,endID      );
+    var dataEnd=  inputData.indexOf(CommTerm);
+    var SortedData = inputData.substring(endID+1, dataEnd);
+    parseout.TaskID =ID;
+    parseout.SortedData =SortedData;
+}
 server.listen(9911);
